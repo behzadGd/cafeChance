f19f463c5e8883252845d9c4adf62320d20510d3
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-03 16:20:05 +0330

    bg change pic for user page

--- src/Componenets/LoginForm.js
+++ src/Componenets/LoginForm.js
@@ -3,15 +3,15 @@ import Input from "./Input";
 import Button from "./Button";
 function LoginForm() {
     return ( 
-         <div className="flex flex-col justify-center justify-items-center  mt-28">
-                <h1 className="font-bold mb-5 flex justify-center">ورود کاربر</h1>
+         <div className="flex flex-col justify-center justify-items-center  mt-28 bg-pink-400 bg-opacity-30 h-3/4 rounded-3xl">
+                <h1 className="font-bold mb-5 flex justify-center text-xl text-black">ورود کاربر</h1>
                 <Input type="text" placeholder="نام کاربری" className="input input-bordered input-primary w-full max-w-xs my-4 " />
                 <Input type="password" placeholder="رمز عبور" className="input input-bordered input-primary w-full max-w-xs"/>
                 <div className="flex flex-row-reverse m-4 mt-8">
-                    <Button className='btn btn-outline btn-secondary mx-2 w-32 '>ورود</Button>
-                <Button className='btn btn-outline btn-secondary mx-2 w-32' >ثبت نام</Button>
+                    <Button className='btn btn-outline text-black mx-2 w-32 '>ورود</Button>
+                <Button className='btn btn-outline  text-black mx-2 w-32' >ثبت نام</Button>
                 </div>
-                <h2 className="flex justify-end">
+                <h2 className="flex justify-end text-black">
                     فراموشی رمز عبور؟
                 </h2>
             </div>
--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -7,11 +7,11 @@ function UserPage() {
     const [tab,setTab]=useState('login')
     console.log(tab)
     return (
-        <div className="flex justify-around">
-            <div className="flex justify-center w-96 h-96  mt-28 bg-slate-700" >
-                this is image
-            </div>
+        <div className="flex justify-around bg-cover bg-[url('../public/img/5.jpg')] w-full h-screen">
             {tab === 'login'?<LoginForm/> : <RejesterForm/>}
+            <div className="flex justify-center w-96 h-96  mt-28 " >
+            </div>
+            
         </div>
     )
 }
