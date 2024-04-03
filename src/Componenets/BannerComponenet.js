c578ba8c50d647da6f292de79f225a93105aedc4
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-03 15:55:27 +0330

    detais of banner

--- src/Componenets/BannerComponenet.js
+++ src/Componenets/BannerComponenet.js
@@ -4,11 +4,23 @@ import Button from './Button'
 function BannerComponenet() {
   return (
     <>
-    <div className='w-full h-96'>
-      <div className="bg-[url('../public/img/2.png')] bg-cover w-full h-full flex justify-center items-end">
-                <div className='mb-7 '>
-                  <Button className='btn btn-outline mx-3 bg-stone-800'>رزرو آنلاین</Button>
+    <div className='w-full h-screen flex'>
+      <div className="bg-[url('../public/img/3.jpg')] bg-cover w-full h-full flex justify-end items-end ">
+               
+               
+                <div className='w-1/2 h-96 flex-col content-end item-end'>
+                 
+                 
+                  <div className='w-2/4 mb-2   '>
+                  <h1 className='font-extrabold text-8xl text-black text-right  mb-5'>کافه چنس</h1>
+                  <h2 className='text-xl text-black font-bold text-right mr-16 mt-3'>شانستو برای مزه بهتر امتحان کن</h2>
+                </div>
+               
+               
+                <div className='mb-32  w-2/5 '>
+                  <Button className='btn btn-outline mx-3 bg-stone-800 mt-9 '>رزرو آنلاین</Button>
                 <Button className='btn btn-outline bg-stone-800'>مشاهده منو</Button>
+                </div>
                 </div>
         
     </div>
--- src/Componenets/Header.js
+++ src/Componenets/Header.js
@@ -3,10 +3,10 @@ import { navBarLinks } from "../Libs/Constans";
 import { NavLink } from "react-router-dom";
 function Header() {
     return ( 
-        <div className="flex justify-center h-14 items-center">
+        <div className="flex justify-center h-14 items-center bg-lime-950">
             <div className="flex flex-row-reverse">
                 {navBarLinks.map((item,index)=>{
-                   return <NavLink to={item.path} className={({ isActive }) => isActive ? "mx-2 text-2xl text-black" : "mx-2 text-2xl"} key={index}>{item.title}</NavLink>
+                   return <NavLink to={item.path} className={({ isActive }) => isActive ? "mx-2 text-2xl text-black" : "mx-2 text-2xl text-white"} key={index}>{item.title}</NavLink>
                 })}
             </div>
         </div>
