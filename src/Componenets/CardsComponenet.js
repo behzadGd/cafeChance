15a948aab5c93d281bbb2ec44da6041e25c850e6
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-17 01:17:14 +0330

    up date style

--- src/Componenets/CardsComponenet.js
+++ src/Componenets/CardsComponenet.js
@@ -4,10 +4,10 @@ function CardsComponenet() {
     <div className="card w-96 glass m-7 mt-30">
   <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
   <div className="card-body">
-    <h2 className="card-title">Life hack</h2>
-    <p>How to park your car at your garage?</p>
+    <h2 className="card-title text-custom_darkbrown">Life hack</h2>
+    <p className="text-custom_darkbrown">How to park your car at your garage?</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Learn now!</button>
+      <button className="btn bg-custom_darkbrown text-custom_brown">Learn now!</button>
       
     </div>
   </div>
