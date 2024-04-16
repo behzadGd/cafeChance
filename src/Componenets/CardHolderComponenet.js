a41f65d7c2951e390df96f8296303d7f0d49e32b
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-17 01:16:54 +0330

    up date style

--- src/Componenets/CardHolderComponenet.js
+++ src/Componenets/CardHolderComponenet.js
@@ -3,7 +3,10 @@ import CardsComponenet from "./CardsComponenet";
 function CardHolderComponenet() {
     return ( <>
     {/* <CardsComponenet/> */}
-    <h1 className="text-center font-extrabold text-4xl text-custom_darkbrown m-10">انواع قهوه های کافه چنس</h1>
+        <div className="flex justify-center ">
+                <h2 className="text-center font-extrabold text-4xl text-custom_brown m-10 bg-custom_darkbrown w-96 p-2 rounded-tl-custom-soft-small">انواع قهوه های کافه چنس</h2>
+
+        </div>
     <div className="flex justify-center">
         {Array.from(Array(3)).map((item,index)=>{
         console.log(index,item)
