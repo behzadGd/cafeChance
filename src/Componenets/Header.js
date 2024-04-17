061329e342c38d28b588ccd8e96dfe240b1ca26c
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-18 02:46:13 +0330

    up date style logo

--- src/Componenets/Header.js
+++ src/Componenets/Header.js
@@ -6,13 +6,13 @@ function Header() {
     return ( 
         <div className="flex justify-between h-16 items-center bg-custom_darkbrown">
             {/* admin icon instagram icon */}
-            <div className="flex justify-center  h-4/6 ">
+            <div className="flex justify-center items-center  h-4/6 bg-custom_brown rounded-tl-custom-soft-small p-2 cursor-pointer">
                 <SunIcon/>
-                    <Link className="-mt-0.5" to={'/user'}>
+                    <Link className=" m-2" to={'/user'}>
                     <img className="w-10 h-10 " src="https://img.icons8.com/quill/100/user-male-circle.png" alt="user-male-circle"/>  
-                    {/* <i class="fas fa-search"></i>              */}
                 </Link>
-    
+                        <img className="w-9 h-9" src="https://img.icons8.com/pastel-glyph/512/000000/search--v3.png" alt="search--v3"/>
+
            
             </div>
             {/* nav bar */}
@@ -24,7 +24,7 @@ function Header() {
                 })}
             </div>
             {/* logo */}
-            <div className="bg-[url('../public/img/logochance.png')] w-14 h-14 bg-cover mr-6 mb-2"></div>
+            <div className="bg-[url('../public/img/logochance.png')] w-14 h-16 bg-cover mr-6 mb-2"></div>
         </div>
      );
 }
