55de3d322300ffcfb07f9dd3e3b8439464bf6ba3
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-18 02:47:00 +0330

    update row 4 and 5

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,12 +3,16 @@ import Button from "../Componenets/Button";
 import BannerComponenet from "../Componenets/BannerComponenet";
 import CardHolderComponenet from "../Componenets/CardHolderComponenet";
 import ModernBanner from "../Componenets/ModernBanner";
+import GallaryComponenet from "../Componenets/GallaryComponenet";
+import IconRow from "../Componenets/IconRow";
 function HomePage() {
     return (
         <>
             <BannerComponenet/>
             <CardHolderComponenet/>
             <ModernBanner/>
+            <GallaryComponenet/>
+            <IconRow/>
         </>
     )
 }
