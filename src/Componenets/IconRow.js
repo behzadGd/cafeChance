04dedc40380bf16824aed809d4378ef3ec880683
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-18 02:46:38 +0330

    add row 5 home page website

---
+++ src/Componenets/IconRow.js
@@ -0,0 +1,29 @@
+import React from "react";
+function IconRow() {
+    return ( 
+    <>
+    <div className="flex justify-center items-center">
+        <h5 className="text-center font-extrabold text-4xl text-custom_brown mt-5 bg-custom_darkbrown w-96 p-2 rounded-tl-custom-soft-small">بر پایه های اسپرسو</h5>
+        </div>
+         <div className="flex justify-center">
+            <div className=" border-custom_darkbrown border-2 w-3/4 border-dashed ">
+            <img src="img/cap 6.png" />
+        </div>
+        {/* <div>
+            <img src="img/cap 1.png" />
+        </div>
+        <div>
+            <img src="img/cap 3.png" />
+        </div>
+        <div>
+            <img src="img/cap 5.png" />
+        </div>
+        <div>
+            <img src="img/cap 4.png" />
+        </div>  */}
+    </div>
+    </>
+        );
+}
+
+export default IconRow;
