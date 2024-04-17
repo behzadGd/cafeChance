3687a6a2c7760dbeccf9720df7d423bb07c5bb1d
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-18 02:46:01 +0330

    row 4 home page website

---
+++ src/Componenets/GallaryComponenet.js
@@ -0,0 +1,11 @@
+import React from "react";
+function GallaryComponenet() {
+    return ( 
+        // gallary row 4 home page
+        <div className="bg-custom_darkbrown w-full h-screen mt-5 flex justify-center">
+            <h4 className="text-center font-extrabold text-4xl text-custom_darkbrown mt-4 bg-custom_brown w-96 p-2 rounded-tl-custom-soft-small">گالری تصاویر کافه چنس</h4>
+        </div>
+     );
+}
+
+export default GallaryComponenet;
