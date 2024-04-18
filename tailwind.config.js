d7e388deca7ad0e399a0ebd9c34479cfe4a73ffc
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-18 07:15:12 +0330

    Up date fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      body: ['Yekan', 'sans-serif', ],
+      display: ['Yekan', 'sans-serif',],
+    },
     extend: {
        colors: {
       'custom_brown':'#a47e50',
