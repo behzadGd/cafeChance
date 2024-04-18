ec8b10e4b9df3a08a3f452f88d2bef59587fe9d8
behzadGd <seyed.behzad.ghaemmaghami@gmail.com>
2024-04-18 07:16:11 +0330

    up date icon footer

--- src/Componenets/Footer.js
+++ src/Componenets/Footer.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 function Footer() {
     return ( <>
     <div className="bg-custom_darkbrown w-full h-72 flex justify-between">
-        <div className="w-1/3 flex-col justify-center items-center bg-black"> 
-        <Button className={'btn btn-wide'}>خانه</Button>
-        <Button className={'btn btn-wide my-2'}>منو</Button>
-        <Button className={'btn btn-wide'}>درباره ما</Button>
+        <div className="w-1/3 flex-col-reverse justify-center items-start "> 
+        <div className="">
+            <Link to={'/'}>
+        <div className="text-center m-2 mt-6">خانه</div>
+        </Link>
+        <Link to={'/product'}>
+        <div className="text-center m-2">منو</div>
+        </Link>
+        <Link to={'/about'}>
+        <div className="text-center m-2">درباره ما</div>
+        </Link>
+        <Link to={'/user'}>
+        <div className="text-center m-2">حساب کاربری</div>
+        </Link>
+        </div>
+        {/* social media */}
+        <div className="flex justify-center mt-7">
+        <img className="w-12 h-12" src="https://img.icons8.com/color/96/whatsapp--v1.png" alt="whatsapp--v1"/>
+        <img  className="w-12 h-12"  src="https://img.icons8.com/fluency/96/instagram-new.png" alt="instagram-new"/>
+        <img  className="w-12 h-12"  src="https://img.icons8.com/color/96/google-maps.png" alt="google-maps"/>
+        </div>
         </div>
          {/* google map */}
         <div className="w-1/3 flex justify-center items-center">google map</div>
